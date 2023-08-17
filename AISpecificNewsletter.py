e55d11b20f48e38d7fbe699d22ecbc8d66f194b7
Jax-Hax <jaxhacks543@gmail.com>
2023-08-17 09:34:33 -0400

    initial stuff

---
+++ AISpecificNewsletter.py
@@ -0,0 +1,12 @@
+from functions import *
+# Newsletters
+# sign in with gmail
+
+# fetch todays emails
+
+# check the email and run different things depending on it, so for TLDR say remove sponsors, summarize everything, include the link (with normal code do this yourself)
+# AI Papers
+# https://ai.papers.bar/papers/recent/
+
+# Articles
+# techcrunch
